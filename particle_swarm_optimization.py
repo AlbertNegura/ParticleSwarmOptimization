7ea1e6054f74cfa2fc43efbc16058afe1ec78e3a
Albert Negura <Albert.Negura@Gmail.com>
2021-02-02 10:08:21 +0100

    Basic refactoring

--- particle_swarm_optimization.py
+++ particle_swarm_optimization.py
@@ -15,8 +15,6 @@ class PSO():
     args = []
     kwargs = {}
 
-    particle_output = False
-
     swarmsize = None
     iterations = None
     rand_init = None
@@ -26,7 +24,6 @@ class PSO():
     T1 = None
     T2 = None
     CONVERGENCE = None
-    DEBUG = None
     PROCESSES = None
 
     function = None
@@ -42,7 +39,7 @@ class PSO():
 
     scale_factor = None
 
-    def __init__(self, mode='regular', swarmsize = 100, iterations = 100, rand_init = False, omega = 0.5, c1 = 0.5, c2 = 0.5, T1 = 1e-10, T2 = 1e-10, CONVERGENCE = False, DEBUG = False, PROCESSES = 1, function = 0):
+    def __init__(self, mode='regular', swarmsize = 100, iterations = 100, rand_init = False, omega = 0.5, c1 = 0.5, c2 = 0.5, T1 = 1e-10, T2 = 1e-10, CONVERGENCE = False, PROCESSES = 1, function = 0):
         if mode != 'regular':
             self.swarmsize = swarmsize
             self.iterations = iterations
@@ -53,7 +50,6 @@ class PSO():
             self.T1 = T1
             self.T2 = T2
             self.CONVERGENCE = CONVERGENCE
-            self.DEBUG = DEBUG
             self.PROCESSES = PROCESSES
 
             self.function = function
@@ -72,7 +68,6 @@ class PSO():
             self.T1 = pso.getfloat("step_convergence_threshold")
             self.T2 = pso.getfloat("value_convergence_threshold")
             self.CONVERGENCE = pso.getboolean("converge_early")
-            self.DEBUG = pso.getboolean("DEBUG")
             self.PROCESSES = pso.getint("PROCESSES")
 
             self.function = config['functions'].getint("function_selection")
@@ -145,44 +140,6 @@ class PSO():
 
         return z
 
-    def plot_figure(self):
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        x = np.arange(np.min(self.lower_bounds), np.max(self.upper_bounds), 0.05)
-        y = np.arange(np.min(self.lower_bounds), np.max(self.upper_bounds), 0.05)
-        X, Y = np.meshgrid(x,y)
-        zs = np.array(self.function_of(np.ravel(X),np.ravel(Y)))
-        Z = zs.reshape(X.shape)
-
-        ax.plot_surface(X,Y,Z, cmap='viridis', edgecolor='none')
-        plt.title("3D Plot of Objective Function")
-        plt.show()
-
-    def plot_contour(self):
-        fig, ax = plt.subplots()
-
-        if np.max(self.upper_bounds) > 0 and np.min(self.lower_bounds) < 0:
-            x = np.arange(np.min(self.lower_bounds)*2, np.max(self.upper_bounds)*2, 0.05)
-            y = np.arange(np.min(self.lower_bounds)*2, np.max(self.upper_bounds)*2, 0.05)
-        elif np.min(self.lower_bounds) < 0 and np.max(self.upper_bounds) < 0:
-            x = np.arange(np.min(self.lower_bounds), 0-np.max(self.upper_bounds), 0.05)
-            y = np.arange(np.min(self.lower_bounds), 0-np.max(self.upper_bounds), 0.05)
-        elif np.min(self.lower_bounds) > 0 and np.max(self.upper_bounds) > 0:
-            x = np.arange(abs(np.min(self.lower_bounds))+np.min(self.lower_bounds), 2*np.max(self.upper_bounds), 0.05)
-            y = np.arange(abs(np.min(self.lower_bounds))+np.min(self.lower_bounds), 2*np.max(self.upper_bounds), 0.05)
-        else:
-            x = np.arange(2*np.min(self.lower_bounds), abs(np.max(self.upper_bounds))+np.max(self.upper_bounds), 0.05)
-            y = np.arange(2*np.min(self.lower_bounds), abs(np.max(self.upper_bounds))+np.max(self.upper_bounds), 0.05)
-
-        X, Y = np.meshgrid(x,y)
-        zs = np.array(self.function_of(np.ravel(X),np.ravel(Y)))
-        Z = zs.reshape(X.shape)
-
-        CS = ax.contour(X,Y,Z, cmap='viridis', edgecolor='none')
-        ax.clabel(CS, inline=1, fontsize=10)
-        plt.title("2D Contour Plot of Objective Function")
-        plt.show()
-
     def pso(self):
         #global mp_pool
         #global swarmsize
@@ -194,9 +151,6 @@ class PSO():
         upperV = np.abs(ub-lb)
         lowerV = -upperV
 
-        #plot_figure()
-        #plot_contour()
-
         objective = partial(self._obj_wrapper, self.error, self.args, self.kwargs)
 
         if self.PROCESSES > 1:
@@ -204,12 +158,11 @@ class PSO():
             mp_pool = multiprocessing.Pool(self.PROCESSES)
 
         #initialize a few arrays
-        S = self.swarmsize
-        x = np.random.rand(S,2)
+        x = np.random.rand(self.swarmsize,2)
         v = np.zeros_like(x)
         p = np.zeros_like(x)
-        fx = np.zeros(S)
-        fp = np.ones(S)*np.inf
+        fx = np.zeros(self.swarmsize)
+        fp = np.ones(self.swarmsize)*np.inf
         g = []
         fg = np.inf
 
@@ -220,7 +173,7 @@ class PSO():
         if self.PROCESSES>1:
             fx = np.array(self.mp_pool.map(objective,x))
         else:
-            for i in range(S):
+            for i in range(self.swarmsize):
                 fx[i] = objective(x[i,:])
 
         i_update = fx<fp
@@ -234,14 +187,14 @@ class PSO():
         else:
             g = x[0,:].copy()
 
-        v=lowerV + np.random.rand(S,2)*(upperV-lowerV)
+        v=lowerV + np.random.rand(self.swarmsize,2)*(upperV-lowerV)
 
         it = 1
         while it <= self.iterations:
             self.x_hist[it-1] = np.array(x)
             self.v_hist[it-1] = np.array(v)
-            rp = np.random.uniform(size=(S,2))
-            rg = np.random.uniform(size=(S,2))
+            rp = np.random.uniform(size=(self.swarmsize,2))
+            rg = np.random.uniform(size=(self.swarmsize,2))
 
             v = self.omega*v + self.c1*rp*(p-x) + self.c2*rg*(g-x)
 
@@ -255,7 +208,7 @@ class PSO():
             if self.PROCESSES>1:
                 fx = np.array(mp_pool.map(objective,x))
             else:
-                for i in range(S):
+                for i in range(self.swarmsize):
                     fx[i] = objective(x[i,:])
 
 
@@ -267,9 +220,6 @@ class PSO():
             self.min_cost_function[it-1]=fp[i_min]
             self.avg_cost_function[it-1]=np.average(fp)
             if fp[i_min] < fg:
-                if self.DEBUG:
-                    print('New best for swarm at iteration {:}: {:} {:}' \
-                          .format(it, p[i_min, :], fp[i_min]))
 
                 p_min = p[i_min, :].copy()
                 stepsize = np.sqrt(np.sum((g - p_min) ** 2))
@@ -299,17 +249,8 @@ class PSO():
                 else:
                     g = p_min.copy()
                     fg = fp[i_min]
-
-            if self.DEBUG:
-                print('Best after iteration {:}: {:} {:}'.format(it, g, fg))
             it += 1
 
-        print('Stopping search: maximum iterations reached --> {:}'.format(self.iterations))
-
-        if self.particle_output:
-            return g, fg, p, fp
-        else:
-            return g, fg
 
     def animate2D(self, data_used, label):
         #global ax1, data, line, stop, ani
